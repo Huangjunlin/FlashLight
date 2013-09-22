ddec8da4994507b63b8a9bf62019dabc6d86b7d0
MrJun <junlinhuang1@gmail.com>
2013-09-22 23:53:37 +0800

    更新
    增加服务，在服务中开启闪光灯

--- src/com/king/flashlight/MainActivity.java
+++ src/com/king/flashlight/MainActivity.java
@@ -1,32 +1,26 @@
 package com.king.flashlight;
 
-import android.hardware.Camera;
-import android.hardware.Camera.Parameters;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.KeyEvent;
-import android.view.Menu;
+import android.content.Intent;
 import android.view.View;
-import android.view.WindowManager;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
 	private Button light_bt;
 	private boolean isopen = false;
-	private Camera camera;
-	private Parameters pm;
+	private Intent intent;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		//±£³ÖÆÁÄ»»½ÐÑ
+		 
+		/*//±£³ÖÆÁÄ»»½ÐÑ
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, 
-				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-		
+				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);*/
+		intent = new Intent(MainActivity.this,LightService.class);
 		light_bt = (Button) findViewById(R.id.bt_light);
 		light_bt.setOnClickListener(this);
 	}
@@ -34,36 +28,22 @@ public class MainActivity extends Activity implements OnClickListener {
 	
 	@Override
 	public void onClick(View view) {
+		
 		if (!isopen) {
+			MainActivity.this.startService(intent);
 			light_bt.setBackgroundResource(R.drawable.shou_on);
-			camera = Camera.open();
-			pm = camera.getParameters();
-			pm.setFlashMode(Parameters.FLASH_MODE_TORCH);
-			camera.setParameters(pm);
 			isopen = true;
 		}else {
+			MainActivity.this.stopService(intent);
 			light_bt.setBackgroundResource(R.drawable.shou_off);
-			pm.setFlashMode(Parameters.FLASH_MODE_OFF);
-			camera.setParameters(pm);
-			camera.release();
-			camera = null;
 			isopen = false;
 		}
 		
 	}
 	
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		// TODO Auto-generated method stub
-		return super.onKeyDown(keyCode, event);
-	}
+
 	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+
 
 	
 
